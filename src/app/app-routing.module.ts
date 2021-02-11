f18d4c9da7c4303613d530bb86ce4a4ec8705dff
EduardoZarzosaGallardo <68938242+EduardoZarzosaGallardo@users.noreply.github.com>
2021-02-11 11:31:45 -0600

    Register & Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { from } from 'rxjs';
+import { AuthModule } from 'src/app/auth/auth.module'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/auth', pathMatch: 'full'},
+  {path: 'auth', loadChildren: 'src/app/auth/auth.module#AuthModule'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
